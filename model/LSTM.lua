e84402fdfe9551cc7574776e0987d0b2832a3a95
Adam Paszke <adam.paszke@gmail.com>
2015-07-30 01:41:14 +0200

    Peephole-like thing in LSTM

--- model/LSTM.lua
+++ model/LSTM.lua
@@ -23,8 +23,11 @@ function LSTM.lstm(input_size, output_size, rnn_size, n, dropout)
     local prev_c = inputs[L*2]
     -- the input to this layer
     if L == 1 then
-      x = inputs[1]
-      input_size_L = input_size
+    --   x = nn.Linear(input_size, rnn_size)(inputs[1])
+    --   x = nn.ReLU()(x)
+    --   input_size_L = rnn_size
+    x = inputs[1]
+    input_size_L = input_size
     else
       x = outputs[(L-1)*2]
       if dropout > 0 then x = nn.Dropout(dropout)(x):annotate({ name = "Dropout in layer: " .. L }) end -- apply dropout, if any
@@ -33,9 +36,11 @@ function LSTM.lstm(input_size, output_size, rnn_size, n, dropout)
     -- evaluate the input sums at once for efficiency
     local i2h = nn.Linear(input_size_L, 4 * rnn_size)(x):annotate({ name = "i2h in layer: " .. L })
     local h2h = nn.Linear(rnn_size, 4 * rnn_size)(prev_h):annotate({ name = "h2h in layer: " .. L })
+    -- local c2h = nn.Linear(rnn_size, 3 * rnn_size)(prev_c)
     local all_input_sums = nn.CAddTable()({i2h, h2h}):annotate({ name = "i2h and h2h sum in layer: " .. L })
     -- decode the gates
     local sigmoid_chunk = nn.Narrow(2, 1, 3 * rnn_size)(all_input_sums):annotate({ name = "sigmoid narrow in layer: " .. L })
+    -- sigmoid_chunk = nn.CAddTable()({c2h, sigmoid_chunk})
     sigmoid_chunk = nn.Sigmoid()(sigmoid_chunk):annotate({ name = "sigmoid in layer: " .. L })
     local in_gate = nn.Narrow(2, 1, rnn_size)(sigmoid_chunk):annotate({ name = "in gate in layer: " .. L })
     local forget_gate = nn.Narrow(2, rnn_size + 1, rnn_size)(sigmoid_chunk):annotate({ name = "forget gate in layer: " .. L })

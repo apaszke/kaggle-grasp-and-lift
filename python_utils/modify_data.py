5660eaec7e99f0e7120ee678148851a2b547291f
Adam Paszke <adam.paszke@gmail.com>
2015-07-30 02:20:51 +0200

    python2 -> python3

--- python_utils/modify_data.py
+++ python_utils/modify_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import argparse
 from subprocess import call
+from functools import reduce
 
 parser = argparse.ArgumentParser(description='Filter out 0 labels from the training set')
 parser.add_argument('-n', default=-1, type=int, help='how many files to filter', dest='num_files')
@@ -8,10 +9,10 @@ parser.add_argument('-c', default=False, action='store_true', help='clear filter
 args = parser.parse_args()
 
 if args.should_rm:
-    print 'removing old files'
+    print('removing old files')
     call(['rm', '-rf', 'data/filtered'])
     call(['mkdir', 'data/filtered'])
-    print 'done'
+    print('done')
 
 data_in_path = "data/train/subj{0}_series{1}_data.csv"
 events_in_path = "data/train/subj{0}_series{1}_events.csv"
@@ -24,10 +25,10 @@ offset = 8
 total_samples = 0
 total_used_samples = 0
 # we want inclusive ranges
-for subj in xrange(1, num_subjects + 1):
-    for series in xrange(1, num_series + 1):
+for subj in range(1, num_subjects + 1):
+    for series in range(1, num_series + 1):
         # load files
-        print 'reading files for subject {}, series {}'.format(subj, series)
+        print('reading files for subject {}, series {}'.format(subj, series))
         data_df = pd.read_csv(data_in_path.format(subj, series))
         events_df = pd.read_csv(events_in_path.format(subj, series))
 
@@ -36,14 +37,14 @@ for subj in xrange(1, num_subjects + 1):
         events_df['id'] = events_df['id'].map(lambda x: int(x.split('_')[2]))
 
         # find event indices
-        print 'filtering events'
+        print('filtering events')
         hs_df = events_df[events_df['HandStart'] != 0].id
         fdt_df = events_df[events_df['FirstDigitTouch'] != 0].id
         bslp_df = events_df[events_df['BothStartLoadPhase'] != 0].id
         lo_df = events_df[events_df['LiftOff'] != 0].id
         r_df = events_df[events_df['Replace'] != 0].id
         br_df = events_df[events_df['BothReleased'] != 0].id
-        num_events = hs_df.count() / 150
+        num_events = int(hs_df.count() / 150)
 
         # check if it's one of the strange files
         counts = [
@@ -55,20 +56,20 @@ for subj in xrange(1, num_subjects + 1):
             br_df.count()
         ]
         file_is_ok = True
-        for i in xrange(1, len(counts)):
+        for i in range(1, len(counts)):
             if counts[0] != counts[i]:
                 file_is_ok = False
 
         if not file_is_ok or counts[0] % 150 != 0:
-            print 'there is a problem with this file...'
+            print('there is a problem with this file...')
             continue
 
         # the file has to be ok now
-        print 'found: ' + str(num_events) + ' events'
+        print('found: ' + str(num_events) + ' events')
 
         # event_boundaries is a list of pairs (t_start, t_end)
         event_boundaries = []
-        for i in xrange(0, num_events * 150, 150):
+        for i in range(0, num_events * 150, 150):
             event_boundaries.append((hs_df.iloc[i] - offset, hs_df.iloc[i] + 150 + offset))
             event_boundaries.append((fdt_df.iloc[i] - offset, fdt_df.iloc[i] + 150 + offset))
             event_boundaries.append((bslp_df.iloc[i] - offset, bslp_df.iloc[i] + 150 + offset))
@@ -77,21 +78,21 @@ for subj in xrange(1, num_subjects + 1):
             event_boundaries.append((br_df.iloc[i] - offset, br_df.iloc[i] + 150 + offset))
 
         # print some information
-        event_lengths = map(lambda (s, e): e - s + 1, event_boundaries)
+        event_lengths = map(lambda ev: ev[1] - ev[0] + 1, event_boundaries)
         used_samples = reduce(lambda x, y: x + y, event_lengths)
         avg_length = used_samples / num_events / 6
         percent_used = float(used_samples) / events_df['id'].count() * 100
         total_samples += events_df['id'].count()
         total_used_samples += used_samples
-        print 'using {} samples ({:.2f}%)'.format(used_samples, percent_used)
-        print 'average event length: {}'.format(avg_length)
+        print('using {} samples ({:.2f}%)'.format(used_samples, percent_used))
+        print('average event length: {}'.format(avg_length))
         sparsity = 1 - (150.) / avg_length
-        print 'sparsity: {}'.format(sparsity)
+        print('sparsity: {}'.format(sparsity))
 
         # extract only selected ranges
         data_slices = []
         events_slices = []
-        for i in xrange(0, num_events):
+        for i in range(0, num_events):
             start, end = event_boundaries[i]
             data_slices.append(data_df.iloc[start:end+1])
             events_slices.append(events_df.iloc[start:end+1])
@@ -100,10 +101,10 @@ for subj in xrange(1, num_subjects + 1):
         pd.concat(data_slices).to_csv(data_out_path.format(subj, series), index=False)
         pd.concat(events_slices).to_csv(events_out_path.format(subj, series), index=False)
 
-        print 'done'
+        print('done')
 
         if args.num_files > -1 and ((subj - 1) * num_series) + series >= args.num_files:
             quit()
 
 total_percent_used = float(total_used_samples) / total_samples * 100
-print 'used {} samples ({:.2f}%)'.format(total_used_samples, total_percent_used)
+print('used {} samples ({:.2f}%)'.format(total_used_samples, total_percent_used))
